9af7fcdd912610ae06bbf786020f68e7ab569ebe
mamaevila(maeve) <93776026+maivilya@users.noreply.github.com>
2023-02-25 19:04:52 +0500

    метод заполнения массива случайными числами

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,33 @@
+import java.util.Random;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        int[] arr = fill(14, 1, 70);
+
+    }
+
+
+
+    private static int[] fill(int len, int min, int max) {
+        int[] array = new int[len];
+        Random random = new Random();
+        for (int i = 0; i < len; i++) {
+            array[i] = random.nextInt((max - min) + 1) + min;
+        }
+        return array;
     }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
