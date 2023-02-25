44cd99bb1a072ddaf734395b118870aed3ad566d
mamaevila(maeve) <93776026+maivilya@users.noreply.github.com>
2023-02-25 19:19:29 +0500

    heapify(преобразование в двоичную кучу)

--- src/Main.java
+++ src/Main.java
@@ -9,6 +9,26 @@ public class Main {
 
     }
 
+    private static void heapify(int[] arr, int n, int i){
+        int largest = i;
+        int left = 2*i + 1;
+        int right = 2*i + 2;
+
+        if (left < n && arr[left] > arr[largest]) {
+            largest = left;
+        }
+        if (right < n && arr[right] > arr[largest]) {
+            largest = right;
+        }
+        if (largest != i) {
+            int temp = arr[i];
+            arr[i] = arr[largest];
+            arr[largest] = temp;
+
+            heapify(arr, n, largest);
+        }
+    }
+
     private static void print(int[] arr) {
         int length = arr.length;
         System.out.print("[");
@@ -20,8 +40,6 @@ public class Main {
             }
         }
     }
-
-
     private static int[] fill(int len, int min, int max) {
         int[] array = new int[len];
         Random random = new Random();
